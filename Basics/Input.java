79d09361d37f4c7a2d58233287549b9ed8e2abd9
Snehal Singh <snehalsingh.0407@gmail.com>
2025-07-30 22:23:04 +0530

    new file added

---
+++ Basics/Input.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Input {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        int number = sc.nextInt();
+        System.out.println("You entered: " + number);
+        sc.close();
+        }
+
+    }
+
+
+//Inputs in java
+//nextLine() - Reads a line of text
+//nextInt() - Reads an integer
+//nextDouble() - Reads a double
+//nextBoolean() - Reads a boolean value
+// next() - Reads a single word
+// nextFloat() - Reads a float
+// nextLong() - Reads a long integer
+// nextShort() - Reads a short integer
+// nextByte() - Reads a byte
+// nextBigInteger() - Reads a BigInteger
+// nextBigDecimal() - Reads a BigDecimal
