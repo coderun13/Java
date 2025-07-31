b82fef96f94226950b0ffb55e51da33d0fdc60cb
Snehal Singh <snehalsingh.0407@gmail.com>
2025-08-01 01:17:17 +0530

    file modified

---
+++ Basics/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        int number = 10;
+        double decimalNumber = 20.5;
+        char letter = 'A';
+        // String text = "Hello, World!";
+
+        // Print the variables
+        System.out.println("Integer: " + number);
+        System.out.println("Double: " + decimalNumber);
+        System.out.println("Character: " + letter);
+        // System.out.println("String: " + text);
+    }
+}
