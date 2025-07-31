9c67df1c59da1c24c3628f03fdc3ce780e5082f3
Snehal Singh <snehalsingh.0407@gmail.com>
2025-08-01 01:14:11 +0530

    modified file

---
+++ Basics/datatypes.java
@@ -0,0 +1,34 @@
+public class datatypes {
+    public static void main(String[] args) {
+        byte b= 8;
+        System.out.println(b);
+        char ch = 'A';
+        System.out.println(ch);
+        short s = 16;
+        System.out.println(s);
+        int i = 32;
+        System.out.println(i);
+        long l = 64L;
+        System.out.println(l);
+        float f = 3.14f;
+        System.out.println(f);
+        double d = 3.14159;
+        System.out.println(d);
+        boolean bool = true;
+        System.out.println(bool);
+        
+    }
+}
+
+
+// Data Types in Java
+// 1. Primitive Data Types
+//    - int: Integer data type
+//    - double: Double-precision floating-point
+//    - char: Character data type
+//    - boolean: Boolean data type (true/false)
+
+// 2. Non-Primitive Data Types
+//    - String: Sequence of characters
+//    - Arrays: Collection of similar data types
+//    - Classes: User-defined data types
